8ea029412107302f3e10f9737a4492c86b7ec5e5
Suphanat Chunhapanya <haxx.pop@gmail.com>
2025-06-20 20:26:37 +0700

    Put TLS certificates into the connections

--- host.go
+++ host.go
@@ -6,10 +6,19 @@ import (
 	"crypto/ed25519"
 	"crypto/rand"
 	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
 	"net"
 	"net/netip"
+	"sync"
+	"time"
 
 	quic "github.com/quic-go/quic-go"
+
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 const (
@@ -48,8 +57,15 @@ func NewHost(ctx context.Context, opts ...HostOption) (*Host, error) {
 	h.tr = &quic.Transport{
 		Conn: conn,
 	}
+
+	// Create a self-signed certifiate from the identity private key
+	if h.crt, err = createTLSCertFromKey(h.sk); err != nil {
+		return nil, err
+	}
+	// Assign the certificate to the TLS config and require client authentication
 	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{tls.Certificate{}},
+		Certificates: []tls.Certificate{*h.crt},
+		ClientAuth:   tls.RequireAnyClientCert,
 	}
 	h.ln, err = h.tr.Listen(tlsConfig, &quic.Config{})
 	if err != nil {
@@ -63,20 +79,34 @@ func NewHost(ctx context.Context, opts ...HostOption) (*Host, error) {
 
 func (h *Host) Connect(ctx context.Context, addr net.Addr) error {
 	tlsConfig := &tls.Config{
-		InsecureSkipVerify: true, // TODO: Verify the certifcate properly when it's implemented
+		Certificates:       []tls.Certificate{*h.crt}, // Put a certificate to do client authentication
+		InsecureSkipVerify: true,                      // TODO: Verify the certifcate properly when it's implemented
 	}
 	conn, err := h.tr.Dial(ctx, addr, tlsConfig, &quic.Config{})
 	if err != nil {
 		return err
 	}
 
+	log.Infof("connected to %s", addr)
+
 	// TODO
 	_ = conn
 	return nil
 }
 
+type ConnectionHandler func(peer.ID, net.Addr)
+
+func (h *Host) SetConnectionHandler(handler ConnectionHandler) {
+	h.handlerLk.Lock()
+	defer h.handlerLk.Unlock()
+
+	h.handler = handler
+}
+
 // processLoop handles all incoming connections
 func (h *Host) processLoop(ctx context.Context) {
+	log.Infof("listening for incoming connections at %s", h.ep)
+
 	for {
 		conn, err := h.ln.Accept(ctx)
 		if err != nil {
@@ -105,9 +135,54 @@ func WithIdentity(sk crypto.PrivateKey) HostOption {
 }
 
 type Host struct {
-	ep *net.UDPAddr
-	sk crypto.PrivateKey
+	crt *tls.Certificate
+	ep  *net.UDPAddr
+	sk  crypto.PrivateKey
 
 	tr *quic.Transport
 	ln *quic.Listener
+
+	handlerLk sync.RWMutex
+	handler   ConnectionHandler
+}
+
+// createTLSCertFromKey creates a self-signed certificate from a private key
+func createTLSCertFromKey(key crypto.PrivateKey) (*tls.Certificate, error) {
+	var pub crypto.PublicKey
+
+	// Currently only ed25519 is supported in this function
+	switch k := key.(type) {
+	case ed25519.PrivateKey:
+		pub = k.Public()
+	default:
+		return nil, fmt.Errorf("unsupported key type: %T", key)
+	}
+
+	tmpl := x509.Certificate{
+		SerialNumber: big.NewInt(1),                      // This can be anything
+		Subject:      pkix.Name{CommonName: "localhost"}, // This can be anything
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, pub, key)
+	if err != nil {
+		return nil, err
+	}
+
+	keyBytes, err := x509.MarshalPKCS8PrivateKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: keyBytes})
+
+	cert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return nil, err
+	}
+	return &cert, nil
 }
