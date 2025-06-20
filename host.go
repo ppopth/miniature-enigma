fa507763da3640f7f2e146a455eec332800bddc3
Suphanat Chunhapanya <haxx.pop@gmail.com>
2025-06-21 04:03:43 +0700

    Read peer IDs from TLS certificates

--- host.go
+++ host.go
@@ -18,6 +18,7 @@ import (
 
 	quic "github.com/quic-go/quic-go"
 
+	ic "github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
 )
 
@@ -46,7 +47,10 @@ func NewHost(ctx context.Context, opts ...HostOption) (*Host, error) {
 		if err != nil {
 			return nil, err
 		}
-		h.sk = sk
+		err = WithIdentity(sk)(h)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// Listen for the specified endpoint
@@ -87,14 +91,30 @@ func (h *Host) Connect(ctx context.Context, addr net.Addr) error {
 		return err
 	}
 
-	log.Infof("connected to %s", addr)
+	// Derive the peer id from the certificate
+	peerCrt := conn.ConnectionState().TLS.PeerCertificates[0]
+	pid, err := parsePeerIDFromCerticate(peerCrt)
+	if err != nil {
+		return fmt.Errorf("failed parsing for a peer ID from the TLS certificate: %v", err)
+	}
 
-	// TODO
-	_ = conn
+	log.Infof("connected to %s at %s", pid, addr)
+
+	go h.handler(pid, conn)
 	return nil
 }
 
-type ConnectionHandler func(peer.ID, net.Addr)
+type DgramConnection interface {
+	SendDatagram(payload []byte) error
+	ReceiveDatagram(context.Context) ([]byte, error)
+
+	LocalAddr() net.Addr
+	RemoteAddr() net.Addr
+}
+
+// TODO: The connection handler is supposed to be able to handle any type of connection, but
+// we support only a datagram connection for now.
+type ConnectionHandler func(peer.ID, DgramConnection)
 
 func (h *Host) SetConnectionHandler(handler ConnectionHandler) {
 	h.handlerLk.Lock()
@@ -106,17 +126,27 @@ func (h *Host) SetConnectionHandler(handler ConnectionHandler) {
 // processLoop handles all incoming connections
 func (h *Host) processLoop(ctx context.Context) {
 	log.Infof("listening for incoming connections at %s", h.ep)
+	log.Infof("my peer id is %s", h.pid)
 
 	for {
 		conn, err := h.ln.Accept(ctx)
 		if err != nil {
 			// If there is an error, it probably means the context has been cancelled
-			log.Warnf("quic-go listener returns an error in the Host processLoop: %v", err)
+			log.Warnf("quic-go listener returned an error in the Host processLoop: %v", err)
 			return
 		}
 
-		// TODO
-		_ = conn
+		// Derive the peer id from the certificate
+		peerCrt := conn.ConnectionState().TLS.PeerCertificates[0]
+		pid, err := parsePeerIDFromCerticate(peerCrt)
+		if err != nil {
+			log.Warnf("failed parsing for a peer ID from the TLS certificate: %v", err)
+			continue
+		}
+
+		log.Infof("accepted a connection from %s at %s", pid, conn.RemoteAddr())
+
+		go h.handler(pid, conn)
 	}
 }
 
@@ -129,7 +159,28 @@ func WithAddrPort(ep netip.AddrPort) HostOption {
 
 func WithIdentity(sk crypto.PrivateKey) HostOption {
 	return func(h *Host) error {
+		var pubkey ic.PubKey
+		var privkey ic.PrivKey
+		var err error
+
+		// Currently only ed25519 is supported in this function
+		switch k := sk.(type) {
+		case ed25519.PrivateKey:
+			privkey, err = ic.UnmarshalEd25519PrivateKey(k)
+			pubkey = privkey.GetPublic()
+		default:
+			return fmt.Errorf("unsupported key type: %T", sk)
+		}
+		if err != nil {
+			return err
+		}
+		p, err := peer.IDFromPublicKey(pubkey)
+		if err != nil {
+			return err
+		}
+
 		h.sk = sk
+		h.pid = p
 		return nil
 	}
 }
@@ -137,6 +188,7 @@ func WithIdentity(sk crypto.PrivateKey) HostOption {
 type Host struct {
 	crt *tls.Certificate
 	ep  *net.UDPAddr
+	pid peer.ID
 	sk  crypto.PrivateKey
 
 	tr *quic.Transport
@@ -185,4 +237,29 @@ func createTLSCertFromKey(key crypto.PrivateKey) (*tls.Certificate, error) {
 		return nil, err
 	}
 	return &cert, nil
+}
+
+// parsePeerIDFromCerticate derive the peer ID from a TLS certifiate
+func parsePeerIDFromCerticate(crt *x509.Certificate) (peer.ID, error) {
+	crtPubkey := crt.PublicKey
+
+	var pubkey ic.PubKey
+	var err error
+
+	// Currently only ed25519 is supported in this function
+	switch k := crtPubkey.(type) {
+	case ed25519.PublicKey:
+		pubkey, err = ic.UnmarshalEd25519PublicKey(k)
+	default:
+		return "", fmt.Errorf("unsupported public key type: %T", crtPubkey)
+	}
+	if err != nil {
+		return "", err
+	}
+
+	pid, err := peer.IDFromPublicKey(pubkey)
+	if err != nil {
+		return "", err
+	}
+	return pid, nil
 }
